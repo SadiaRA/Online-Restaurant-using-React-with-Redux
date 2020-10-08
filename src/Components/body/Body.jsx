cdfd6bc5211ca67337d5fd6a1c43915e522c04a8
Sadia Rahnuma Ashraf <sadiaashraf013@gmail.com>
2020-10-08 19:43:57 +0200

    Added route and reactstrap component like navtoggler,modal etc

--- src/Components/body/Body.jsx
+++ src/Components/body/Body.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Menu from "./Menu";
+import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
-import Home from "./Home";
+
+import { Route, Redirect } from "react-router-dom";
 const Body = () => {
   return (
     <div>
-      <Menu />
+      <Route path="/" exact component={Home} />
+      <Route path="/menu" exact component={Menu} />
+      <Route path="/about" exact component={About} />
+      <Route path="/contact" exact component={Contact} />
+      <Redirect from="/" to="/home" />
     </div>
   );
 };
--- src/Components/body/Menu.jsx
+++ src/Components/body/Menu.jsx
@@ -2,15 +2,25 @@ import React, { Component } from "react";
 import Dishes from "../../data/Dishes";
 import MenuItem from "./MenuItem";
 import DishDetails from "./DishDetails";
+import { CardColumns, Modal, ModalBody, ModalFooter, Button } from "reactstrap";
 
 class Menu extends Component {
   state = {
     dishes: Dishes,
     selectedDish: null,
+    modalOpen: false,
   };
 
   onDishSelect = (dish) => {
-    this.setState({ selectedDish: dish });
+    this.setState({
+      selectedDish: dish,
+      modalOpen: !this.state.modalOpen,
+    });
+  };
+  modalClick = () => {
+    this.setState({
+      modalOpen: !this.state.modalOpen,
+    });
   };
 
   render() {
@@ -27,8 +37,15 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-6">{menu}</div>
-          <div className="col-6">{dishDetail}</div>
+          <CardColumns>{menu}</CardColumns>
+          <Modal isOpen={this.state.modalOpen} onClick={this.modalClick}>
+            <ModalBody>{dishDetail}</ModalBody>
+            <ModalFooter>
+              <Button color="danger" onClick={this.modalClick}>
+                Close
+              </Button>
+            </ModalFooter>
+          </Modal>
         </div>
       </div>
     );
